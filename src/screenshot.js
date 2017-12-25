c5ebc9d82fa925edd1e7396a59c3f26e8d76a2eb
Alex Page <alex@alexpage.com.au>
2017-12-26 09:08:10 +1100

    fixing screenshots

--- src/screenshot.js
+++ src/screenshot.js
@@ -20,7 +20,6 @@ const Puppeteer = require( 'puppeteer' );
 // Local
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-console.log( Puppeteer.launch() );
 
 /**
  * Screenshot - Take a screenshot of an array of URLs
@@ -29,29 +28,37 @@ console.log( Puppeteer.launch() );
  *
  * @return {string}            - Resolves the contents of the file
  */
-const Screenshot = async ( url, width, height ) => {
-
-	// return new Promise( ( resolve, reject ) => {
-	// 	resolve();
-	// })
+const Screenshot = async ( urls, width, height ) => {
 
 	const browser = await Puppeteer.launch(); // Start puppeteer instance
 	const page    = await browser.newPage();  // Create a new page
 
-	await page.goto( url );
+	for ( let url of urls ) {
 
-	const dimensions = {
-		height: height || await page.evaluate( () => document.documentElement.offsetHeight ),
-		width:  width || 1200,
-	};
+		try {
+			await page.goto( url );
 
-	page.setViewport( dimensions ); // Apply the dimensions to the page
+			const dimensions = {
+				height: height || await page.evaluate( () => document.documentElement.offsetHeight ),
+				width:  width || 1200,
+			};
 
-	const filename = ( url.replace(/(^\w+:|^)\/\//, '' ) ).replace( '/', '__');
+			page.setViewport( dimensions ); // Apply the dimensions to the page
 
-	await page.screenshot({ path: `__tests__/tmp/${ filename }[${ dimensions.width }].png` });
+			const filename = ( url.replace(/(^\w+:|^)\/\//, '' ) ).replace( '/', '__');
+
+			await page.screenshot({ path: `screenshots/${ filename }[${ dimensions.width }].png` });
+		}
+		catch( error ) {
+			return error;
+		}
+
+
+	}
 
 	await browser.close();
+
 };
 
-Screenshot( 'https://alexpage.com.au' );
+
+Screenshot( [ 'http://alexpage.com.au', 'https://github.com/alex-page' ] );
